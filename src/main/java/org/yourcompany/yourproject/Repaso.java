990fd9ad2a5d7617c7babc32ffcaeb5accccfcf5
Juan Camilo Liberato Sierra <juan.liberato@estudiantesunibague.edu.co>
2026-02-13 15:22:56 -0500

    correciòn

--- src/main/java/org/yourcompany/yourproject/Repaso.java
+++ src/main/java/org/yourcompany/yourproject/Repaso.java
@@ -4,18 +4,16 @@
 
 package org.yourcompany.yourproject;
 
-/**
- *
- * @author angel
- */
 public class Repaso {
 
     public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+
         Bibliotecario bibliotecario = new Bibliotecario("1", "pepe","pepe@gmail.com","mañana");
-        System.out.println(bibliotecario.darEmail());
+       
 
         Socio socio = new Socio("1", "Juan","juan@gmail.com",5);
-        socio.registrarPrestamo(2);
+        biblioteca.agregarSocio(socio);
 
     }
 }
